5c9da28befe34188d256d994bc998e3cfa7104fe
M-Konvalinka <MikeKonvalinka@gmail.com>
2018-11-19 21:15:33 -0800

    this is a test project I am building along side a udemyReact tutorial, this repo is for the ability to transition between two computers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+       // Below would be the data set, usually from DB in real app
+      {name: 'Mike', age: 24},
+      {name: 'Frank', age: 37},
+      {name: 'Random', age: 69}
+    ],
+    otherState: 'some other value',
+    showPersons: false
+  }
+
+  deletePersonHandler = () => {
+
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: 'Mike', age: 24},
+        {name: event.target.value, age: 37},
+        {name: 'Random', age: 8000}
+    ]
+  })
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
   render() {
+    const style ={
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
+    let persons = null;
+
+    if (this.state.showPersons){
+      persons = (
+        <div>
+          {this.state.persons.map(person => {
+            return <Person 
+            click={this.deletePersonHandler}
+            name={person.name} 
+            age={person.age}/>
+          })}
+        </div>
+      );
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi I'm a react app</h1>
+        <p>This is really working</p>
+        <button 
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
+            
       </div>
     );
   }
