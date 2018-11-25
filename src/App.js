1b8c4cd8536fa4967e1e4137de8ac289eda48ce4
M-Konvalinka <MikeKonvalinka@gmail.com>
2018-11-24 23:48:14 -0800

    added dynamic stayling as well as radium

--- src/App.js
+++ src/App.js
@@ -6,26 +6,40 @@ class App extends Component {
   state = {
     persons: [
        // Below would be the data set, usually from DB in real app
-      {name: 'Mike', age: 24},
-      {name: 'Frank', age: 37},
-      {name: 'Random', age: 69}
+      {id: '1',name: 'Mike', age: 24},
+      {id: '2',name: 'Frank', age: 37},
+      {id: '3',name: 'Random', age: 69}
     ],
     otherState: 'some other value',
     showPersons: false
   }
 
-  deletePersonHandler = () => {
-
+  deletePersonHandler = (personIndex) => {
+    // by slicing below with no arguments you make a copy and are not mutating the original object
+    // const persons = this.state.persons.slice();
+    // ES6 way done below
+    // create copy and update that copy then update original state with new copy
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        {name: 'Mike', age: 24},
-        {name: event.target.value, age: 37},
-        {name: 'Random', age: 8000}
-    ]
-  })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      // below creates the personindex into a new object so not changing the original
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons} );
   }
 
   togglePersonsHandler = () => {
@@ -35,7 +49,8 @@ class App extends Component {
 
   render() {
     const style ={
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -47,19 +62,37 @@ class App extends Component {
     if (this.state.showPersons){
       persons = (
         <div>
-          {this.state.persons.map(person => {
+          {this.state.persons.map((person, index) => {
             return <Person 
-            click={this.deletePersonHandler}
+            click={() => this.deletePersonHandler(index)}
             name={person.name} 
-            age={person.age}/>
+            age={person.age}
+            key={person.id}
+            changed={(event) => this.nameChangedHandler(event,person.id)}/>
           })}
         </div>
       );
+
+      style.backgroundColor = 'red';
     }
+
+    // by adding the .join(' ') it turns the array into a string "red bold"
+    // let classes = ['red','bold'].join(' ');
+
+    let classes = [];
+    if(this.state.persons.length <= 2){
+      classes.push('red'); // classes = ['red']
+    }
+    if (this.state.persons.length <= 1){
+      classes.push('bold'); // classes = ['red','bold']
+    }
+
+
     return (
       <div className="App">
         <h1>Hi I'm a react app</h1>
-        <p>This is really working</p>
+        {/* classes as a class name becomes classes.join b/c classes is a array not a string anymore */}
+        <p className={classes.join(' ')}>This is really working</p>
         <button 
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
