01e33e6dd664ce5de167461f5bd848ebf055d63d
M-Konvalinka <MikeKonvalinka@gmail.com>
2018-11-26 19:42:18 -0800

    pushing changes to an application before ejecting

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import Radium from 'radium';
 
 import './Person.css';
 
 const person = (props) => {
+    const style = {
+        // radium understands this due to it being a string that it can parse
+        '@media(min-width: 500px)':{
+            width: '450px'
+        }
+    }
     return(
-        <div className="Person">
+        <div className="Person" style={style}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
         </div>
     )
+    // using radium here for scope or dynamic content
 };
 
-export default person;
+// Need to wrap the export in radium anywhere you plan on using it 
+export default Radium(person);
